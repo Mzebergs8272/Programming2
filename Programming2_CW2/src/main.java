16f229a1bae832411a474b3ff5ea84417f58bf89
unknown <zebergsm@gmail.com>
2025-04-15 14:04:28 +0100

    .

--- Programming2_CW2/src/main.java
+++ Programming2_CW2/src/main.java
@@ -1,17 +1,20 @@
-//import com.opencsv.CSVReader;
-//import com.opencsv.CSVWriter;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 
 import javax.swing.*;
-import javax.swing.table.JTableHeader;
-import javax.swing.table.TableColumn;
-import javax.swing.table.TableColumnModel;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Vector;
 
 
 class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         InventoryManagerApp inventoryManager = new InventoryManagerApp();
         inventoryManager.width = 1920;
         inventoryManager.height = 1080;
@@ -57,11 +60,14 @@ class InventoryManagerApp {
     private String windowTitle;
     public int navBarWidth;
 
-    public ArrayList<Record> displayedRecords;
-    public ArrayList<SalesRecord> salesRecords;
-    public ArrayList<InventoryRecord> inventoryRecords;
+    public ArrayList<ArrayList<String>> salesRecords;
+    public ArrayList<ArrayList<String>> inventoryRecords;
 
     public DefaultTableModel tableModel;
+
+    public JPanel containerTableEdits;
+    public JPanel containerNavBar;
+    public JPanel containerTable;
 
     InventoryManagerApp() {
         window = new JFrame(windowTitle);
@@ -70,119 +76,114 @@ class InventoryManagerApp {
         width = Toolkit.getDefaultToolkit().getScreenSize().width;
         height = Toolkit.getDefaultToolkit().getScreenSize().height;
         window.setSize(width, height-50);
+        salesRecords = new ArrayList<>();
+        inventoryRecords = new ArrayList<>();
     }
 
 
-    public void run() {
+    public void run() throws IOException {
         drawNavigation();
-        drawTableEditSection();
+        drawTableEdits();
         drawTable();
+        drawPageTitle("Stock");
+
+        loadSalesRecords();
+        loadInventoryRecords();
+
         window.setVisible(true);
     }
 
+    void drawPageTitle(String title) {
+        if (!(containerTableEdits.getComponent(0) instanceof JLabel)) {
+            JLabel lblTitle = new JLabel(title);
+            lblTitle.setFont(new Font("Tahoma", Font.BOLD, 40));
+            lblTitle.setForeground(Color.WHITE);
+            containerTableEdits.add(lblTitle, 0);
+        }
+        else {
+            ((JLabel) containerTableEdits.getComponent(0)).setText(title);
+        }
+    }
+
     void drawNavigation() {
-        JPanel navBar = new JPanel();
-        navBar.setPreferredSize(new Dimension(navBarWidth, height-100));
-        navBar.setBackground(Color.RED);
+        containerNavBar = new JPanel();
+        containerNavBar.setPreferredSize(new Dimension(navBarWidth, height-100));
+        containerNavBar.setBackground(Color.RED);
 
         JButton btnInventoryPage = new JButton("Inventory");
         btnInventoryPage.setPreferredSize(new Dimension(new Dimension(navBarWidth, 50)));
+        btnInventoryPage.setBackground(Color.WHITE);
         JButton btnSalesPage = new JButton("Sales");
         btnSalesPage.setPreferredSize(new Dimension(new Dimension(navBarWidth, 50)));
+        btnSalesPage.setBackground(Color.WHITE);
         JButton btnInventoryReport = new JButton("Inventory Report");
         btnInventoryReport.setPreferredSize(new Dimension(navBarWidth, 50));
+        btnInventoryReport.setBackground(Color.WHITE);
         JButton btnSalesReport = new JButton("Sales Report");
         btnSalesReport.setPreferredSize(new Dimension(navBarWidth, 50));
+        btnSalesReport.setBackground(Color.WHITE);
 
-        btnInventoryPage.addActionListener(e -> loadSalesPage());
-        btnSalesPage.addActionListener(e -> loadInventoryPage());
-        btnInventoryReport.addActionListener(e -> drawInventoryReport());
-        btnSalesReport.addActionListener(e -> drawSalesReport());
+        btnInventoryPage.addActionListener(e -> {
+            try {
+                loadInventoryPage();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+        btnSalesPage.addActionListener(e -> {
+            try {
+                loadSalesPage();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+        btnInventoryReport.addActionListener(e -> drawWinInventoryReport());
+        btnSalesReport.addActionListener(e -> drawWinSalesReport());
 
-        navBar.add(btnInventoryPage);
-        navBar.add(btnSalesPage);
-        navBar.add(btnSalesReport);
-        navBar.add(btnInventoryReport);
+        containerNavBar.add(btnInventoryPage);
+        containerNavBar.add(btnSalesPage);
+        containerNavBar.add(btnSalesReport);
+        containerNavBar.add(btnInventoryReport);
 
-        window.add(navBar, BorderLayout.WEST);
+        window.add(containerNavBar, BorderLayout.WEST);
     }
 
-    void drawTableEditSection() {
-        JPanel tempNameContainer = new JPanel();
-        tempNameContainer.setPreferredSize(new Dimension(width, 100));
-        tempNameContainer.setBackground(Color.BLUE);
+    void drawTableEdits() {
+        containerTableEdits = new JPanel();
+        containerTableEdits.setPreferredSize(new Dimension(width, 100));
+        containerTableEdits.setBackground(Color.BLUE);
 
         JButton btnAddRecord = new JButton("Add Record");
-        btnAddRecord.setPreferredSize(new Dimension(new Dimension(200, 50)));
+        btnAddRecord.setPreferredSize(new Dimension(200, 50));
         JButton btnRemoveRecord = new JButton("Remove Record");
-        btnRemoveRecord.setPreferredSize(new Dimension(new Dimension(200, 50)));
+        btnRemoveRecord.setPreferredSize(new Dimension(200, 50));
         JTextField txtSearch = new JTextField();
         txtSearch.setPreferredSize(new Dimension(200, 50));
 
         btnAddRecord.addActionListener(e -> addRecord());
-        btnRemoveRecord.addActionListener(e -> removeRecord());
+        btnRemoveRecord.addActionListener(e -> drawWinRemoveRecord());
         txtSearch.addActionListener(e -> searchTable());
 
-        tempNameContainer.add(btnAddRecord);
-        tempNameContainer.add(btnRemoveRecord);
-        tempNameContainer.add(txtSearch);
-        window.add(tempNameContainer, BorderLayout.NORTH);
+        containerTableEdits.add(btnAddRecord);
+        containerTableEdits.add(btnRemoveRecord);
+        containerTableEdits.add(txtSearch);
+        window.add(containerTableEdits, BorderLayout.NORTH);
     }
 
     void drawTable() {
-        JPanel tableContainer = new JPanel();
-        tableContainer.setPreferredSize(new Dimension(width - navBarWidth, height-100));
-        tableContainer.setBackground(Color.green);
+        containerTable = new JPanel();
+        containerTable.setPreferredSize(new Dimension(width - navBarWidth, height-225));
+        containerTable.setBackground(Color.green);
 
-        // create 2d data array from data in csv
-        String[][] data = {
-                {"Product ID", "Product Name", "Description", "Cost", "Quantity", "Total Value"},
-                {"1", "Cheese", "description", "", "", ""},
-                {"2", "Cheese", "description", "", "", ""},
-                {"2", "Cheese", "description", "", "", ""},
-                {"2", "Cheese", "description", "", "", ""},
-                {"2", "Cheese", "description", "", "", ""},
-                {"2", "Cheese", "description", "", "", ""},
-                {"2", "Cheese", "description", "", "", ""},
-                {"2", "Cheese", "description", "", "", ""},
-                {"2", "Cheese", "description", "", "", ""},
-                {"2", "Cheese", "description", "", "", ""},
-                {"2", "Cheese", "description", "", "", ""},
-                {"2", "Cheese", "description", "", "", ""},
-                {"2", "Cheese", "description", "", "", ""},
-                {"2", "Cheese", "description", "", "", ""},
-
-                {"2", "Cheese", "description", "", "", ""},
-                {"2", "Cheese", "description", "", "", ""},
-                {"2", "Cheese", "description", "", "", ""},
-                {"2", "Cheese", "description", "", "", ""},
-                {"2", "Cheese", "description", "", "", ""},
-                {"2", "Cheese", "description", "", "", ""},
-                {"2", "Cheese", "description", "", "", ""},
-                {"2", "Cheese", "description", "", "", ""},
-                {"2", "Cheese", "description", "", "", ""},
-                {"2", "Cheese", "description", "", "", ""},
-                {"2", "Cheese", "description", "", "", ""},
-                {"2", "Cheese", "description", "", "", ""},
-                {"2", "Cheese", "description", "", "", ""},
-                {"2", "Cheese", "description", "", "", ""},
-                {"2", "Cheese", "description", "", "", ""},
-                {"2", "Cheese", "description", "", "", ""},
-                {"2", "Cheese", "description", "", "", ""},
-                {"2", "Cheese", "description", "", "", ""},
-                {"2", "Cheese", "description", "", "", ""},
-                {"2", "Cheese", "description", "", "", ""},
-                {"2", "Cheese", "description", "", "", ""},
-
-        };
-        String[] columnNames = {"Item ID", "Item Name", "Description", "Cost", "Quantity", "Total Value"};
-
-        tableModel = new DefaultTableModel(data, columnNames);
+        tableModel = new DefaultTableModel();
         JTable tblRecords = new JTable(tableModel);
         tblRecords.setRowHeight(30);
-        tblRecords.setPreferredSize(new Dimension(1425, height));
-        tableContainer.add(tblRecords);
-        window.add(tableContainer);
+
+        JScrollPane scrollPane = new JScrollPane(tblRecords);
+        scrollPane.setPreferredSize(new Dimension(1425, height-225));
+        containerTable.add(scrollPane);
+
+        window.add(containerTable);
     }
 
     void searchTable() {
@@ -193,25 +194,170 @@ class InventoryManagerApp {
         // creates modal window
         // text fields to specify ID, Name, Description, Cost, Quantity, Total Value
         // access tableModel to add row
-        tableModel.addRow(new String[]{"3", "Cheese", "description", "", "", ""});
+        tableModel.addRow(new String[]{String.valueOf(tableModel.getRowCount()), "", "", "", "", ""});
     }
-    void removeRecord() {
-        // creates modal window
-        // textfield to specify list of rows to remove
-        // textfield to specify a range of rows to remove
-        // access tableModel to remove rows
+
+    void drawWinRemoveRecord() {
+        JFrame winRemoveRecord = new JFrame("Remove Record");
+        winRemoveRecord.setLayout(new FlowLayout());
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        winRemoveRecord.setSize(new Dimension(400, 200));
+
+        JLabel lblRangeRemove = new JLabel("Specify range of records to remove. Example: 1-5");
+        JLabel lblListRemove = new JLabel("Specify list of records to remove. Example: 1 2 3 4 5");
+
+        JTextField txtRangeRemove = new JTextField();
+        txtRangeRemove.setPreferredSize(new Dimension(200, 25));
+        JTextField txtListRemove = new JTextField();
+        txtListRemove.setPreferredSize(new Dimension(200, 25));
+
+        JButton btnRemoveRecord = new JButton("Remove specified records");
+        btnRemoveRecord.addActionListener(e -> {
+
+            ArrayList<String> recordIDs = new ArrayList<String>();
+
+            recordIDs.addAll(Arrays.asList(txtListRemove.getText().split("[, ]")));
+
+            if (txtRangeRemove.getText().matches("[0-9]-[0-9]")) {
+                System.out.println(txtRangeRemove.getText());
+                String[] range = txtRangeRemove.getText().split("-");
+                for (int i = Math.min(Integer.parseInt(range[0]), Integer.parseInt(range[1]));
+                     i <= Math.max(Integer.parseInt(range[0]), Integer.parseInt(range[1]));
+                     i++)
+                {
+                    recordIDs.add(String.valueOf(i));
+                }
+            }
+            System.out.println(recordIDs);
+            // iterates through all records IDs specified and removes corresponding records from tableModel
+            for (String recordID : recordIDs) {
+                Vector<Vector> dataVector = tableModel.getDataVector();
+                for (int i=0; i<dataVector.size(); i++) {
+                    if (dataVector.get(i).elementAt(0).equals(recordID)) {
+                        tableModel.removeRow(i);
+                        i -= 1;
+                    }
+                }
+            }
+        });
+
+        winRemoveRecord.add(lblRangeRemove);
+        winRemoveRecord.add(txtRangeRemove);
+        winRemoveRecord.add(lblListRemove);
+        winRemoveRecord.add(txtListRemove);
+        winRemoveRecord.add(btnRemoveRecord);
+
+        winRemoveRecord.setVisible(true);
+
+    }
+
+    void drawWinAddSale() {
+        JFrame window = new JFrame("Add Sale");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setLayout(new FlowLayout());
+        window.setSize(new Dimension(400, 200));
+
+        JComboBox<ArrayList<String>> dropdown = new JComboBox<ArrayList<String>>();
+
+        for (ArrayList<String> record : inventoryRecords) {
+            dropdown.addItem(record);
+        }
+
+        window.add(dropdown);
+        window.setVisible(true);
+
+    }
+
+    void loadSalesRecords() throws IOException {
+        String content = new String(Files.readAllBytes(Paths.get("Programming2_CW2/src/SalesRecords.json")));
+        JSONArray json = new JSONArray(content);
+
+        String[][] data = new String[json.length()][6];
+
+        for (int i=0; i<json.length(); i++) {
+            JSONObject record = json.getJSONObject(i);
+            int j = 0;
+            System.out.println(json.getJSONObject(i));
+            System.out.println(record.keySet());
+            for (Object col : record.keySet()) {
+                data[i][j] = record.get(col.toString()).toString();
+                salesRecords.add(new ArrayList<String>());
+                salesRecords.get(i).add(data[i][j]);
+                j++;
+            }
+        }
+
+        tableModel.setDataVector(data, new String[]{"Sale ID", "Product ID", "Customer Name", "Date", "Quantity", "Total Value"});
+
+    }
+
+    void loadInventoryRecords() throws IOException {
+        String content = new String(Files.readAllBytes(Paths.get("Programming2_CW2/src/StockRecords.json")));
+        JSONArray json = new JSONArray(content);
+
+        String[][] data = new String[json.length()][6];
+
+        for (int i=0; i<json.length(); i++) {
+            JSONObject record = json.getJSONObject(i);
+            int j = 0;
+            for (Object col : record.keySet()) {
+                data[i][j] = record.get(col.toString()).toString();
+                inventoryRecords.add(new ArrayList<String>());
+                inventoryRecords.get(i).add(data[i][j]);
+                j++;
+            }
+        }
+
+        tableModel.setDataVector(data, new String[]{"Item ID", "Item Name", "Description", "Cost", "Quantity", "Total Value"});
+    }
+
+    void removeSelectedRecords() {
+
     }
 
     // changes ArrayList records to sales records from csv
     // changes text on buttons for corresponding page
-    void loadSalesPage() {
-        // use setDateVector to display different table
-        // tableModel.setDataVector(new String[][] {}, new String[]{});
+    void loadSalesPage() throws IOException {
+        drawPageTitle("Sales");
+        containerNavBar.getComponent(1).setBackground(Color.GREEN);
+        containerNavBar.getComponent(0).setBackground(Color.WHITE);
+
+        try {
+            loadSalesRecords();
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        JButton btnAddSale = new JButton("Add Sale");
+        btnAddSale.addActionListener(e -> drawWinAddSale());
+        btnAddSale.setPreferredSize(new Dimension(200, 50));
+        containerTableEdits.remove(1);
+        containerTableEdits.add(btnAddSale, 1);
 
     }
     // changes records list to inventory records from csv
     // changes display of buttons and nav bar
-    void loadInventoryPage() {}
-    void drawInventoryReport() {}
-    void drawSalesReport() {}
+    void loadInventoryPage() throws IOException {
+        drawPageTitle("Stock");
+        containerNavBar.getComponent(1).setBackground(Color.WHITE);
+        containerNavBar.getComponent(0).setBackground(Color.GREEN);
+        try {
+            loadInventoryRecords();
+        }
+        catch (IOException e) {
+            throw e;
+        }
+
+        JButton btnAddRecord = new JButton("Add Record");
+        btnAddRecord.setPreferredSize(new Dimension(200, 50));
+        btnAddRecord.addActionListener(e -> addRecord());
+        containerTableEdits.remove(1);
+        containerTableEdits.add(btnAddRecord, 1);
+    }
+
+    // draws window displaying graphs for inventory
+    void drawWinInventoryReport() {}
+    // draws window displaying graphs for sales
+    void drawWinSalesReport() {}
 }
